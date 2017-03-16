a15984505aa9732025b81a6bc8971bdc25743c02
Me999999999 <jdspoor@att.net>
2017-03-16 14:08:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$('#the_button').on('click', function(){
+
+})
+$('#show_modal').on('click', function(){
+
+  $('#my_modal').modal();
+
+});
+$('#close_modal').on('click', function(){
+
+  $('#my_modal').modal('hide');
+
+});
